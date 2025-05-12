7debe7f9b13bcaeaeacf341cf92dfc2e1422dbbd
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-05-12 14:47:59 +0100

    first commit

---
+++ farmers-hub/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-500 text-2xl">
+        Welcome to Farmers Hub!
+      </p>
+    </div>
+  );
+}
