04461252c6afee91b830831f4a1dff49787756be
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-11 15:18:09 +0100

    multi tenant setup and implementation

--- farmers-hub/src/modules/home/ui/components/search-filters/category-dropdown.tsx
+++ farmers-hub/src/modules/home/ui/components/search-filters/category-dropdown.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useRef, useState } from "react";
-import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategory-menu";
 import Link from "next/link";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
@@ -21,7 +20,6 @@ export default function CategoryDropdown({
 }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const { getDropdownPosition } = useDropdownPosition(dropdownRef);
 
   const onMouseEnter = () => {
     if (category.subcategories) {
@@ -30,7 +28,6 @@ export default function CategoryDropdown({
   };
 
   const onMouseLeave = () => setIsOpen(false);
-  const dropdownPosition = getDropdownPosition();
 
   // const toggleDropdown = () => {
   //   if (category.subcategories?.docs?.length) {
@@ -70,11 +67,7 @@ export default function CategoryDropdown({
         )}
       </div>
 
-      <SubcategoryMenu
-        category={category}
-        isOpen={isOpen}
-        position={dropdownPosition}
-      />
+      <SubcategoryMenu category={category} isOpen={isOpen} />
     </div>
   );
 }
