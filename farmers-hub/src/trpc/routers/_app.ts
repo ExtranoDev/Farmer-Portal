a9a188424e7141e0ce5b8d7fab88f0f4d607a845
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-12 09:44:39 +0100

    tenant pages design

--- farmers-hub/src/trpc/routers/_app.ts
+++ farmers-hub/src/trpc/routers/_app.ts
@@ -3,9 +3,11 @@ import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { authRouter } from "@/modules/auth/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedure";
 import { tagsRouter } from "@/modules/tags/server/procedure";
+import { tenantsRouter } from "@/modules/tenants/server/procedure";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
+  tenants: tenantsRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });
