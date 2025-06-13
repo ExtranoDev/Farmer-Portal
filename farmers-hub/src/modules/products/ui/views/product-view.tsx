3727af185ba12fae06c16c4f9197e1027fe05deb
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-13 10:34:57 +0100

    cart button design and checkout feature inclusion on the product page

--- farmers-hub/src/modules/products/ui/views/product-view.tsx
+++ farmers-hub/src/modules/products/ui/views/product-view.tsx
@@ -12,11 +12,24 @@ import { LinkIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
+import dynamic from "next/dynamic";
 
 interface Props {
   productId: string;
   tenantSlug: string;
 }
+
+const CartButton = dynamic(
+  () => import("../components/cart-button").then((mod) => mod.CartButton),
+  {
+    ssr: false,
+    loading: () => (
+      <Button className="flex-1 bg-pink-400" disabled>
+        Please wait...
+      </Button>
+    ),
+  }
+);
 
 export const ProductView = ({ productId, tenantSlug }: Props) => {
   const trpc = useTRPC();
@@ -100,9 +113,7 @@ export const ProductView = ({ productId, tenantSlug }: Props) => {
             <div className="border-t lg:border-t-0 lg:border-l h-full">
               <div className="flex flex-col gap-4 p-6 border-b">
                 <div className="flex flex-row items-center gap-2">
-                  <Button variant="elevated" className="flex-1 bg-pink-400">
-                    Add to cart
-                  </Button>
+                  <CartButton productId={productId} tenantSlug={tenantSlug} />
                   <Button
                     className="size-12"
                     variant="elevated"
