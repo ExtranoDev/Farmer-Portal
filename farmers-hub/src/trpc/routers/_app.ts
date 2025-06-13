0eb0e77e550e9cef491375e2c9af7f4d6a4959cd
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-13 13:02:50 +0100

    developed the checkout page layout and design

--- farmers-hub/src/trpc/routers/_app.ts
+++ farmers-hub/src/trpc/routers/_app.ts
@@ -4,12 +4,14 @@ import { authRouter } from "@/modules/auth/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedure";
 import { tagsRouter } from "@/modules/tags/server/procedure";
 import { tenantsRouter } from "@/modules/tenants/server/procedure";
+import { checkoutRouter } from "@/modules/checkout/server/procedure";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
   products: productsRouter,
   categories: categoriesRouter,
+  checkout: checkoutRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
