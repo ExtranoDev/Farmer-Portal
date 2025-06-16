b9c3a961d64f63a1832fbe7d4c43d4b8014e4d75
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-16 13:48:31 +0100

    product library design and implementation

--- farmers-hub/src/trpc/routers/_app.ts
+++ farmers-hub/src/trpc/routers/_app.ts
@@ -5,6 +5,8 @@ import { productsRouter } from "@/modules/products/server/procedure";
 import { tagsRouter } from "@/modules/tags/server/procedure";
 import { tenantsRouter } from "@/modules/tenants/server/procedure";
 import { checkoutRouter } from "@/modules/checkout/server/procedure";
+import { libraryRouter } from "@/modules/library/server/procedure";
+
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
@@ -12,6 +14,7 @@ export const appRouter = createTRPCRouter({
   products: productsRouter,
   categories: categoriesRouter,
   checkout: checkoutRouter,
+  library: libraryRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
