a476aa5b24baf8bcfc1a83723e183edc4f4c5ad4
extranodev <101347375+ExtranoDev@users.noreply.github.com>
2025-06-19 11:38:42 +0100

    Resolving stripe connection to user and split payment inplementation

---
+++ farmers-hub/src/app/(app)/(tenants)/stripe-verify/page.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useTRPC } from "@/trpc/client";
+import { LoaderIcon } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const { mutate: verify } = useMutation(
+    trpc.checkout.verify.mutationOptions({
+      onSuccess: (data) => {
+        window.location.href = data.url;
+      },
+      onError: () => {
+        window.location.href = `/`;
+      },
+    })
+  );
+
+  useEffect(() => {
+    verify();
+  }, [verify]);
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LoaderIcon className="animate-spin text-muted-foreground" />
+    </div>
+  );
+};
+
+export default Page;
